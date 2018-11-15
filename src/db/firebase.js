4a5c4b0dd808b4732c395b7efe60596964fd06fd
nickheal <nick3_4@hotmail.com>
2018-11-15 19:44:18 +0000

    Updated favicon and imported just parts of firebase that are used

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const config = {
   apiKey: 'AIzaSyCfhyTT9bDhmL-CpJu56Xnw5Qa5W1cgpPs',
   authDomain: 'my-tools-e3076.firebaseapp.com',
