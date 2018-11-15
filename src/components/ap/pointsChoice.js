210dc6b064de6fb793a5b8bd7123a073fe8aaed9
nickheal <nick3_4@hotmail.com>
2018-11-15 19:05:07 +0000

    Updates some packages and improved the points score selector

--- src/components/ap/pointsChoice.js
+++ src/components/ap/pointsChoice.js
@@ -42,15 +42,17 @@ const SInput = styled.input`
       color: ${props => props.theme.white};
       position: relative;
 
-      &:after {
-        background-color: ${props => props.theme.white};
-        border-radius: 999em;
-        content: '';
-        left: 0;
-        position: absolute;
-        top: 100%;
-        width: 100%;
-        height: 2px;
+      &::before {
+        transform: translateX(0px);
+      }
+
+      &::after {
+        transform: translateX(0px);
+      }
+
+      &::before,
+      &::after {
+        opacity: 0.5;
       }
     }
   }
@@ -59,12 +61,55 @@ const SLabel = styled.label`
   color: ${props => props.theme.white};
   cursor: pointer;
   display: inline-block;
-  font-size: ${props => props.fontSize * 1.5}rem;
-  margin-bottom: 0.5em;
-  padding: 0.5em 0.5em 0.25em;
+  font-size: ${props => props.theme.fontSize * 1.5}rem;
+  margin: 0.5em 0;
+  padding: 0.5em;
   transition: transform 200ms;
 
+  &::before,
+  &::after {
+    border: solid 1px ${props => props.theme.white};
+    content: '';
+    display: block;
+    height: 100%;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    transition: transform 300ms, opacity 300ms;
+    width: 10px;
+    z-index: -1;
+  }
+
+  &::before {
+    border-bottom-left-radius: 999em;
+    border-right: 0;
+    border-top-left-radius: 999em;
+    left: 10%;
+    transform: translateX(-8px);
+  }
+
+  &::after {
+    border-bottom-right-radius: 999em;
+    border-left: 0;
+    border-top-right-radius: 999em;
+    right: 10%;
+    transform: translateX(8px);
+  }
+
   &:hover {
-    transform: scale(1.1);
+    transform: scale(1.2);
+
+    &::before {
+      transform: translateX(-4px);
+    }
+
+    &::after {
+      transform: translateX(4px);
+    }
+
+    &::before,
+    &::after {
+      opacity: 0.5;
+    }
   }
 `;
